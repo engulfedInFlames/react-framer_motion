a372b6ce144579db6b52a46fe4ebc8521088e7ad
engulfedInFlames <recona97@gmail.com>
2023-03-12 14:39:40 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  *{
+    font-family: 'Noto Sans KR', sans-serif;
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    color:inherit;
+  }
+  body{
+    min-height: 100vh;
+  }
+  
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   );
 }
 
